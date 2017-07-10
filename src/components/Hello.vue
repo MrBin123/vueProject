7c4c162fbbead719a50778792a8f556007a32a7b
jiangbin <18501114684@163.com>
2017-07-10 09:11:53 +0800

    提交项目图片

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,8 +1,11 @@
+
 <template>
+
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
+      
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
@@ -21,6 +24,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'hello',
   data () {
